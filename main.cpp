7d8e119ff0471492d2be43fb1aa59bc532917e8c
root <troysung@163.com>
2020-03-19 21:45:24 +0800

    new

--- main.cpp
+++ main.cpp
@@ -3,18 +3,6 @@
 #include <QApplication>
 #include <QWebEngineSettings>
 
-QString getCommandLineUrlArgument()
-{
-    const QStringList args = QCoreApplication::arguments();
-    if (args.count() > 1) {
-        const QString lastArg = args.last();
-        const bool isValidUrl = QUrl::fromUserInput(lastArg).isValid();
-        if (isValidUrl)
-            return lastArg;
-    }
-    return QString();
-}
-
 int main(int argc, char **argv)
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -26,12 +14,6 @@ int main(int argc, char **argv)
 
     BrowserWindow *window = new BrowserWindow();
     Browser::instance().addWindow(window);
-
-    const QString url = getCommandLineUrlArgument();
-    if (!url.isEmpty())
-        window->loadPage(url);
-    else
-        window->loadHomePage();
 
     return app.exec();
 }
