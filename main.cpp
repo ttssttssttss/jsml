88ed7befcff5c4f061c747a55615d8807a014b7e
root <troysung@163.com>
2020-03-22 23:18:30 +0800

    a

--- main.cpp
+++ main.cpp
@@ -3,18 +3,35 @@
 #include <QApplication>
 #include <QWebEngineSettings>
 
+QString getCommandLineUrlArgument()
+{
+   const QStringList args = QCoreApplication::arguments();
+   if (args.count() > 1) {
+   		const QString lastArg = args.last();
+     	const bool isValidUrl = QUrl::fromUserInput(lastArg).isValid();
+     	if (isValidUrl)
+      	return lastArg;
+   }
+   return QString();
+}
+
+
 int main(int argc, char **argv)
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
 
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(QLatin1String(":simplebrowser.svg")));
+   // app.setWindowIcon(QIcon(QLatin1String(":simplebrowser.svg")));
 
     QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::PluginsEnabled, false);
 
     BrowserWindow *window = new BrowserWindow();
     Browser::instance().addWindow(window);
+	
+		const QString url = getCommandLineUrlArgument();
+		if (!url.isEmpty())
+			window->loadPage(url);
 
     return app.exec();
 }
